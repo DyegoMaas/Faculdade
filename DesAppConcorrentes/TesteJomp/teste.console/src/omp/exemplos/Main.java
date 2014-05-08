96b51a16a2d129e0ab9083c8e68df8617a11f033
Dyego <dyego.maas@gmail.com>
2014-05-08 19:33:57 -0300

    DM: Implementado exemplo de JOMP.

---
+++ DesAppConcorrentes/TesteJomp/teste.console/src/omp/exemplos/Main.java
@@ -0,0 +1,17 @@
+package omp.exemplos;
+
+import jomp.runtime.*;
+public class Main {
+
+	public static void main(String[] args) {
+		int meuId = 0;
+		OMP.setNumThreads(15);
+		
+		//omp parallel private(meuId)
+		{
+			meuId = OMP.getThreadNum();
+			System.out.println("Hello from " + meuId);
+		}
+	}
+
+}
---
+++ DesAppConcorrentes/TesteJomp/teste.console/src/omp/exemplos/Main_jomp.java
@@ -0,0 +1,59 @@
+package omp.exemplos;
+
+import jomp.runtime.*;
+public class Main_jomp {
+
+
+	public static void main(String[] args) {
+		int meuId = 0;
+		OMP.setNumThreads(15);
+
+// OMP PARALLEL BLOCK BEGINS
+{
+  __omp_Class0 __omp_Object0 = new __omp_Class0();
+  // shared variables
+  __omp_Object0.args = args;
+  // firstprivate variables
+  try {
+    jomp.runtime.OMP.doParallel(__omp_Object0);
+  } catch(Throwable __omp_exception) {
+    System.err.println("OMP Warning: Illegal thread exception ignored!");
+    System.err.println(__omp_exception);
+  }
+  // reduction variables
+  // shared variables
+  args = __omp_Object0.args;
+}
+// OMP PARALLEL BLOCK ENDS
+
+	}
+
+// OMP PARALLEL REGION INNER CLASS DEFINITION BEGINS
+private static class __omp_Class0 extends jomp.runtime.BusyTask {
+  // shared variables
+  String [ ] args;
+  // firstprivate variables
+  // variables to hold results of reduction
+
+  public void go(int __omp_me) throws Throwable {
+  // firstprivate variables + init
+  // private variables
+  int meuId;
+  // reduction variables, init to default
+    // OMP USER CODE BEGINS
+
+		{
+			meuId = OMP.getThreadNum();
+			System.out.println("Hello from " + meuId);
+		}
+    // OMP USER CODE ENDS
+  // call reducer
+  // output to _rd_ copy
+  if (jomp.runtime.OMP.getThreadNum(__omp_me) == 0) {
+  }
+  }
+}
+// OMP PARALLEL REGION INNER CLASS DEFINITION ENDS
+
+}
+
