1a788b2e7b7a4db92727c3e067c8ad83e8ea6dde
Dyego <dyego.maas@gmail.com>
2014-08-07 00:12:19 -0300

    DM:0: criada factory de ILogicaSubstituicao.

---
+++ BancoDadosII/SimuladorSGBD/source/app/SimuladorSGBD.Core/GerenciamentoBuffer/Buffer/LogicaSubstituicao/LogicaSubstituicaoFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SimuladorSGBD.Core.GerenciamentoBuffer.Buffer.LogicaSubstituicao
+{
+    public class LogicaSubstituicaoFactory {
+        private readonly Lazy<ILogicaSubstituicao> lru = new Lazy<ILogicaSubstituicao>(() => new LeastRecentlyUsed());
+        private readonly Lazy<ILogicaSubstituicao> mru = new Lazy<ILogicaSubstituicao>(() => new MostRecentlyUsed());
+
+        public ILogicaSubstituicao LRU()
+        {
+            return lru.Value;
+        }
+
+        public ILogicaSubstituicao MRU()
+        {
+            return mru.Value;
+        }
+    }
+}
---
+++ BancoDadosII/SimuladorSGBD/source/testes/SimuladorSGBD.Testes/GerenciamentoBuffer/Buffer/LogicaSubstituicao/LogicaSubstituicaoFactoryTeste.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using SimuladorSGBD.Core.GerenciamentoBuffer;
+using SimuladorSGBD.Core.GerenciamentoBuffer.Buffer.LogicaSubstituicao;
+using Xunit;
+
+namespace SimuladorSGBD.Testes.GerenciamentoBuffer.Buffer.LogicaSubstituicao
+{
+    public class LogicaSubstituicaoFactoryTeste
+    {
+        [Fact]
+        public void requisitando_um_lru()
+        {
+            var factory = new LogicaSubstituicaoFactory();
+            ILogicaSubstituicao lru = factory.LRU();
+
+            lru.Should().NotBeNull();
+            lru.Should().BeOfType<LeastRecentlyUsed>();
+        }
+
+        [Fact]
+        public void requisitando_um_mru()
+        {
+            var factory = new LogicaSubstituicaoFactory();
+            ILogicaSubstituicao mru = factory.MRU();
+
+            mru.Should().NotBeNull();
+            mru.Should().BeOfType<MostRecentlyUsed>();
+        }
+    }
+}
