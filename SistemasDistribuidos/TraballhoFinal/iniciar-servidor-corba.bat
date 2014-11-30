44234b410575aaa1bccc44226639bf2fe5bc952b
Dyego <dyego.maas@gmail.com>
2014-11-30 14:30:43 -0200

    DM: organizando exemplos CORBA.

--- SistemasDistribuidos/TraballhoFinal/iniciar-servidor-corba.bat
+++ SistemasDistribuidos/TraballhoFinal/iniciar-servidor-corba.bat
@@ -1,8 +1,11 @@
-cd C:\dev\Faculdade\SistemasDistribuidos\TraballhoFinal\CORBA\src
 set path=%path%;"C:\Program Files\Java\jdk1.7.0_80\bin"
-idlj -fall corba\boasvindas\BoasVindas.idl
-javac -cp "C:\dev\Faculdade\SistemasDistribuidos\TraballhoFinal\CORBA\src" *\**\*.java
-set classpath=.
+
+cd C:\dev\Faculdade\SistemasDistribuidos\TraballhoFinal\CORBA\src\
+idlj -fall BoasVindas.idl
+
+set classpath=..\bin
+javac -cp "C:\dev\Faculdade\SistemasDistribuidos\TraballhoFinal\CORBA\src" BoasVindas\*.java
 javac *.java
+
 start orbd -ORBInitialPort 2000
-start java servidor.servidor -ORBInitialPort 2000 
+start java BoasVindas.servidor -ORBInitialPort 2000 
