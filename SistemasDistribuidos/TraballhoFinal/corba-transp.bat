72f510fd271412a15f53a8d3fd84323082a4ae2a
Dyego <dyego.maas@gmail.com>
2014-11-30 13:53:07 -0200

    DM: organizando exemplos CORBA e RMI.

--- SistemasDistribuidos/TraballhoFinal/corba-transp.bat
+++ SistemasDistribuidos/TraballhoFinal/corba-transp.bat
@@ -1,8 +1,8 @@
-cd C:\Users\OECKSLERS\Desktop\SD - Working LR1\SD - Corba - LogTransporte\src
-set path=%path%;"C:\Program Files\Java\jdk1.7.0_07\bin"
-idlj -fall LogisticaTransporte.idl
-javac -cp "C:\Users\OECKSLERS\Desktop\SD - Working LR1\SD - Corba - LogTransporte\src" LogisticaTransporte\*.java
+cd C:\dev\Faculdade\SistemasDistribuidos\TraballhoFinal\CORBA
+set path=%path%;"C:\Program Files\Java\jdk1.7.0_80\bin"
+idlj -fall BoasVindas.idl
+javac -cp "C:\dev\Faculdade\SistemasDistribuidos\TraballhoFinal\CORBA" BoasVindas\*.java
 set classpath=.
 javac *.java
 start orbd
-start java LogisticaTransporte.servidor
+start java servidor.servidor
