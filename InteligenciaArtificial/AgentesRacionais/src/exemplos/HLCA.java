b5631b9a04a04caf23e33ebaf56a5be559520f29
Dyego <dyego.maas@gmail.com>
2015-03-06 21:42:08 -0300

    DM:IA: implementação do problema do homem, lobo, carneiro e alface (atravessar o rio).

---
+++ InteligenciaArtificial/AgentesRacionais/src/exemplos/HLCA.java
@@ -0,0 +1,160 @@
+package exemplos;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import busca.BuscaLargura;
+import busca.Estado;
+import busca.Nodo;
+
+public class HLCA implements Estado {	
+	private char homem;
+	private char lobo;
+	private char carneiro;
+	private char alface;
+	private String nomeAcao;
+
+	public HLCA(char ladoHomem, char ladoLobo, char ladoCarneiro, char ladoAlface, String nomeEstado) {
+		this.homem = ladoHomem;
+		this.lobo = ladoLobo;
+		this.carneiro = ladoCarneiro;
+		this.alface = ladoAlface;
+		this.nomeAcao = nomeEstado;
+	}
+	
+	@Override
+	public int custo() {
+		return 1;
+	}
+
+	@Override
+	public boolean ehMeta() {
+		return homem == 'd' && lobo == 'd' && carneiro == 'd' && alface == 'd';
+	}
+
+	@Override
+	public List<Estado> sucessores() {
+		List<Estado> sucessores = new LinkedList<Estado>();
+		
+		levarHomem(sucessores);
+		levarLobo(sucessores);
+		levarCarneiro(sucessores);
+		levarAlface(sucessores);
+		
+		return sucessores;
+	}
+	
+	private void levarAlface(List<Estado> sucessores) {
+		char margemOposta = ladoOposto(homem);
+		
+		HLCA novoEstado = new HLCA(margemOposta, lobo, carneiro, margemOposta, "levarAlface");
+		
+		if(ehValido(novoEstado)){
+			sucessores.add(novoEstado);
+		}
+	}
+
+	private void levarCarneiro(List<Estado> sucessores) {
+		char margemOposta = ladoOposto(homem);
+		
+		HLCA novoEstado = new HLCA(margemOposta, lobo, margemOposta, alface, "levarCarneiro");
+		
+		if(ehValido(novoEstado)){
+			sucessores.add(novoEstado);
+		}
+	}
+
+	private void levarLobo(List<Estado> sucessores) {
+		char margemOposta = ladoOposto(homem);
+		
+		HLCA novoEstado = new HLCA(margemOposta, margemOposta, carneiro, alface, "levarLobo");
+		
+		if(ehValido(novoEstado)){
+			sucessores.add(novoEstado);
+		}
+	}
+
+	private void levarHomem(List<Estado> sucessores) {
+		char margemOposta = ladoOposto(homem);
+		
+		HLCA novoEstado = new HLCA(margemOposta, lobo, carneiro, alface, "levarHomem");
+		
+		if(ehValido(novoEstado)){
+			sucessores.add(novoEstado);
+		}
+	}
+
+	private boolean ehValido(HLCA novoEstado) {
+		if(lobo == carneiro && lobo != homem)
+			return false;
+		
+		if(carneiro == alface && carneiro != homem)
+			return false;
+		
+		return true;
+	}
+
+	private char ladoOposto(char lado) {
+		return (lado == 'e') ? 'd': 'e';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if(o instanceof HLCA){
+			HLCA outro = (HLCA)o;
+			
+			return 
+					outro.alface == this.alface &&
+					outro.carneiro == this.carneiro &&
+					outro.homem == this.homem &&
+					outro.lobo == this.lobo;
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return ("" + homem + lobo + carneiro + alface).hashCode(); 
+	}
+	
+	@Override
+	public String toString() {
+		StringBuffer direita = new StringBuffer();
+		StringBuffer esquerda = new StringBuffer();
+
+		if(homem == 'e')
+			esquerda.append('h');
+		else
+			direita.append('h');
+		
+		if(lobo == 'e')
+			esquerda.append('l');
+		else
+			direita.append('l');
+		
+		if(carneiro == 'e')
+			esquerda.append('c');
+		else
+			direita.append('c');
+		
+		if(alface == 'e')
+			esquerda.append('a');
+		else
+			direita.append('a');
+		
+		return esquerda + "'---" + direita;
+	}
+
+	public static void main(String[] args) {
+		HLCA problema = new HLCA('e', 'e', 'e', 'e', "inicial");
+		
+		BuscaLargura buscaLargura = new BuscaLargura();
+		Nodo nodo = buscaLargura.busca(problema);
+		
+		if(nodo == null)
+			System.out.println("Problema sem solução");
+		else
+			System.out.println("Solução:\n" + nodo.montaCaminho() + "\n");
+	}
+
+}
