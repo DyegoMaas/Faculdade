196f278937f847bb91f01c6d9443662622acb454
Dyego <dyego.maas@gmail.com>
2015-03-15 13:40:31 -0300

    DM:CG:N2: configurando janela de jogo.

--- ComputacaoGrafica/N2/N2/Program.cs
+++ ComputacaoGrafica/N2/N2/Program.cs
@@ -1,19 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+using System;
+using System.Drawing;
 
 namespace N2
 {
     class Program
     {
+
         /// <summary>
         /// Para evitar 100% de uso da CPU http://www.opentk.com/doc/intro/cpu-usage
         /// </summary>
         /// <param name="args"></param>
+        [STAThread]
         static void Main(string[] args)
         {
+            var posicaoInicialJanela = new Point(50, 50);
+            var tamanhoInicialJanela = new Size(800, 800);
+
+            // Creates a 3.0-compatible GraphicsContext with 32bpp color, 24bpp depth
+            // 8bpp stencil
+            using (var gameWindow = new GameWindow(tamanhoInicialJanela.Width, tamanhoInicialJanela.Height, new GraphicsMode(32, 24, 8, 0)))
+            {
+                gameWindow.X = posicaoInicialJanela.X;
+                gameWindow.Y = posicaoInicialJanela.Y;
+
+                gameWindow.Load += (sender, e) =>
+                {
+                    GL.ClearColor(Color.CadetBlue);
+                };
+
+                gameWindow.Resize += (sender, e) =>
+                {
+
+                };
+
+                gameWindow.UpdateFrame += (sender, e) =>
+                {
+
+                };
+
+                gameWindow.RenderFrame += (sender, e) =>
+                {
+                    //configs do OpenGL
+                    GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+                    gameWindow.SwapBuffers();
+                };
+
+                gameWindow.Run(120, 60);
+            }
         }
     }
 }
