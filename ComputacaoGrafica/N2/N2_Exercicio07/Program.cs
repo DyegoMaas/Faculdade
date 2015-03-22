80a064bfc42b998858d61eaa79018545b0187aaf
Dyego <dyego.maas@gmail.com>
2015-03-22 13:08:47 -0300

    DM:CG:N2: falta corrigir o posicionamento do mouse.

--- ComputacaoGrafica/N2/N2_Exercicio07/Program.cs
+++ ComputacaoGrafica/N2/N2_Exercicio07/Program.cs
@@ -43,27 +43,28 @@ namespace N2_Exercicio07
                     circuloMenor = new Circulo(new Vector2(200f), 50f, 120);
                 };
 
-                gameWindow.KeyDown += (sender, e) =>
-                {
-                    switch (e.Key)
-                    {
-                       
-                    }
-                };
-
-                var antigo = new MouseState();
                 var estadoInicialMouse = new MouseState();
+                var estadoAntigoMouse = new MouseState();
                 gameWindow.UpdateFrame += (sender, e) =>
                 {
                     camera.Update();
 
-                    var atual = Mouse.GetState();
-                    if (atual != antigo && antigo != estadoInicialMouse)
+                    var estadoAtualMouse = Mouse.GetState();
+                    if (estadoAtualMouse != estadoAntigoMouse && estadoAntigoMouse != estadoInicialMouse)
                     {
-                        
+                        if (estadoAtualMouse.IsButtonDown(MouseButton.Left))
+                        {
+                            if (circuloMenor.EstahDentro(gameWindow.Mouse.X, gameWindow.Mouse.Y))
+                            {
+                                Console.Write("dentro");
+
+
+                                
+                            }
+                        }
                     }
 
-                    antigo = atual;
+                    estadoAntigoMouse = estadoAtualMouse;
                 };
 
                 gameWindow.RenderFrame += (sender, e) =>
@@ -221,12 +222,15 @@ namespace N2_Exercicio07
     public class Circulo
     {
         public Vector2[] Pontos { get; private set; }
-        public Vector2 Centro { get; set; }
+        public Vector2 Centro { get; private set; }
+        public float Raio { get; private set; }
 
         public Circulo(Vector2 centro, float raio, int numeroPontos)
         {
+            Raio = raio;
             Centro = centro;
             Pontos = new Vector2[numeroPontos];
+
             for (var i = 0; i < numeroPontos; i++)
             {
                 var theta = 2 * (float)Math.PI * i / numeroPontos;
@@ -235,6 +239,17 @@ namespace N2_Exercicio07
 
                 Pontos[i] = new Vector2(x, y);
             }
+        }
+
+        public bool EstahDentro(float x, float y)
+        {
+            var distancia = Distancia(x, y, Centro.X, Centro.Y);
+            return distancia <= Raio;
+        }
+
+        private float Distancia(float x1, float y1, float x2, float y2)
+        {
+            return (float) Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
         }
     }
 }
