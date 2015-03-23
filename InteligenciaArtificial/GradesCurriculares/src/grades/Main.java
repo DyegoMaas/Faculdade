85722918bd4c60a7cfb56d3a81e43b5d109581b8
Dyego <dyego.maas@gmail.com>
2015-03-22 22:09:15 -0300

    DM:IA:T1: refatoração.

--- InteligenciaArtificial/GradesCurriculares/src/grades/Main.java
+++ InteligenciaArtificial/GradesCurriculares/src/grades/Main.java
@@ -58,58 +58,58 @@ public class Main {
 	private final int QUINTA = 4; 
 	private final int SEXTA = 5;
 	
-	private void executar() {
-		
-		Materia[] curso = new Materia[] {
-			//TODO marcar alguns como concluídos
-			new MateriaBuilder(1, "11", "Introdução à Computação").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(1, "12", "Computação Digital").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(1, "13", "Programação de Computadores").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(1, "14", "Universidade, Ciência e Pesquisa").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(1, "15", "Fundamentos Matemáticos para Computação").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+	private Materia[] curso = new Materia[] {
+		//TODO marcar alguns como concluídos
+		new MateriaBuilder(1, "11", "Introdução à Computação").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(1, "12", "Computação Digital").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(1, "13", "Programação de Computadores").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(1, "14", "Universidade, Ciência e Pesquisa").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(1, "15", "Fundamentos Matemáticos para Computação").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
 
-			new MateriaBuilder(2, "21", "Arquitetura de Computadores").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(2, "22", "Lógica para Computação").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(2, "23", "Linguagem Científica").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(2, "24", "Álgebra Linear para Computação").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(2, "25", "Programação Orientada a Objetos I").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(2, "21", "Arquitetura de Computadores").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(2, "22", "Lógica para Computação").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(2, "23", "Linguagem Científica").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(2, "24", "Álgebra Linear para Computação").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(2, "25", "Programação Orientada a Objetos I").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
 
-			new MateriaBuilder(3, "31", "Sistemas Operacionais").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(3, "32", "Algoritmos e Estruturas de Dados").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(3, "33", "Teoria da Computação").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(3, "34", "Estatística Aplicada à Informática").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(3, "35", "Programação Orientada a Objetos II").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(3, "31", "Sistemas Operacionais").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(3, "32", "Algoritmos e Estruturas de Dados").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(3, "33", "Teoria da Computação").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(3, "34", "Estatística Aplicada à Informática").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(3, "35", "Programação Orientada a Objetos II").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
 
-			new MateriaBuilder(4, "41", "Linguagens de Programação").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(4, "42", "Teoria dos Grafos").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(4, "43", "Protocolos de Comunicação de Dados").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(4, "44", "Métodos Quantitativos").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(4, "45", "Desafios Sociais Contemporâneos").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(4, "41", "Linguagens de Programação").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(4, "42", "Teoria dos Grafos").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(4, "43", "Protocolos de Comunicação de Dados").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(4, "44", "Métodos Quantitativos").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(4, "45", "Desafios Sociais Contemporâneos").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
 
-			new MateriaBuilder(5, "51", "Redes de Computadores").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(5, "52", "Compiladores").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(5, "53", "Desenvolvimento de Aplicações Concorrentes e Distribuídas").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(5, "54", "Engenharia de Software").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(5, "55", "Banco de Dados I - SEMIPRESENCIAL-100%").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(5, "51", "Redes de Computadores").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(5, "52", "Compiladores").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(5, "53", "Desenvolvimento de Aplicações Concorrentes e Distribuídas").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(5, "54", "Engenharia de Software").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(5, "55", "Banco de Dados I - SEMIPRESENCIAL-100%").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
 
-			new MateriaBuilder(6, "61", "Sistemas Distribuídos").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(6, "62", "Comportamento Organizacional").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(6, "63", "Banco de Dados II").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(6, "64", "Processo de Software I").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(6, "65", "Disciplina Optativa I - PRÁTICA EM REDES / GENEXUS").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-				
-			new MateriaBuilder(7, "71", "Processo de Software II").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(7, "72", "Disciplina Optativa II - ROBÓTICA").lecionadaNa(TERCA, 1).eNa(TERCA, 2).construir(),
-			new MateriaBuilder(7, "73", "Computação Gráfica").lecionadaNa(QUINTA, 2).eNa(SEXTA, 1).construir(),
-			new MateriaBuilder(7, "74", "Inteligência Artificial").lecionadaNa(QUARTA, 2).eNa(SEXTA, 2).construir(),
-			new MateriaBuilder(7, "75", "Desenvolvimento Web").lecionadaNa(QUARTA, 1).eNa(QUINTA, 1).construir(),
+		new MateriaBuilder(6, "61", "Sistemas Distribuídos").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(6, "62", "Comportamento Organizacional").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(6, "63", "Banco de Dados II").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(6, "64", "Processo de Software I").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(6, "65", "Disciplina Optativa I - PRÁTICA EM REDES / GENEXUS").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+			
+		new MateriaBuilder(7, "71", "Processo de Software II").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(7, "72", "Disciplina Optativa II - ROBÓTICA").lecionadaNa(TERCA, 1).eNa(TERCA, 2).construir(),
+		new MateriaBuilder(7, "73", "Computação Gráfica").lecionadaNa(QUINTA, 2).eNa(SEXTA, 1).construir(),
+		new MateriaBuilder(7, "74", "Inteligência Artificial").lecionadaNa(QUARTA, 2).eNa(SEXTA, 2).construir(),
+		new MateriaBuilder(7, "75", "Desenvolvimento Web").lecionadaNa(QUARTA, 1).eNa(QUINTA, 1).construir(),
 
-			new MateriaBuilder(8, "81", "Sistemas Multimídia").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(8, "82", "Legislação em Informática").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(8, "83", "Banco de Dados II").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(8, "84", "Empreendedor em Informática").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
-			new MateriaBuilder(8, "85", "Disciplina Optativa III - PROGRAMAÇÃO MICROCON").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir()
-		};
+		new MateriaBuilder(8, "81", "Sistemas Multimídia").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(8, "82", "Legislação em Informática").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(8, "83", "Banco de Dados II").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(8, "84", "Empreendedor em Informática").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir(),
+		new MateriaBuilder(8, "85", "Disciplina Optativa III - PROGRAMAÇÃO MICROCON").lecionadaNa(SEGUNDA, 1).eNa(SEGUNDA, 2).construir()
+	};
+	
+	private void executar() {		
 		
 		//TODO processar o algoritmo
 	}
