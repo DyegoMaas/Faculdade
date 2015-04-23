bc5b78a1bbf1fed7b824956330bb9d75a67c239c
Dyego <dyego@inventti.com.br>
2015-04-23 19:51:33 -0300

    DM:DW:0: Criado programa Nancy com CRUD em memória.

---
+++ DesenvolvimentoWeb/TrabalhoApresentacao/source/Nancy/Modules/PessoaModule.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace ApresentacaoNancy.Modules
+{
+    public class PessoaModule : NancyModule
+    {
+        static readonly List<Pessoa> Pessoas = new List<Pessoa>
+        {
+            new Pessoa
+            {
+                Id = 1,
+                Cpf = 7172233964,
+                Nome = "Dyego"
+            },
+            new Pessoa
+            {
+                Id = 2,
+                Cpf = 67533777352,
+                Nome = "Maria"
+            },
+            new Pessoa
+            {
+                Id = 3,
+                Cpf = 48725773968,
+                Nome = "João"
+            }
+        };
+
+        public PessoaModule() : base("/pessoas")
+        {
+            Get["/"] = _ => Pessoas;
+
+            Get["/{id:long}"] = _ =>
+            {
+                var id = (long)_.id;
+                var pessoa = Pessoas.FirstOrDefault(p => p.Id == id);
+                if(pessoa == null)
+                    throw new Exception(string.Format("Pessoa com id {0} não encontrada.", id));
+
+                return pessoa;
+            };
+
+            Post["/"] = _ =>
+            {
+                var pessoa = this.BindAndValidate<Pessoa>();
+                pessoa.Id = Pessoas.Max(p => p.Id + 1);
+
+                Pessoas.Add(pessoa);
+                return true;
+            };
+
+            Put["/{id:long}"] = _ =>
+            {
+                var id = (long)_.id;
+                var pessoa = Pessoas.FirstOrDefault(p => p.Id == id);
+                if (pessoa == null)
+                    throw new Exception(string.Format("Pessoa com id {0} não encontrada.", id));
+
+                var model = this.BindAndValidate<Pessoa>();
+                pessoa.Nome = model.Nome;
+                pessoa.Cpf = model.Cpf;
+
+                return true;
+            };
+
+            Delete["/{id:long}"] = _ =>
+            {
+                var id = (long)_.id;
+                var pessoa = Pessoas.FirstOrDefault(p => p.Id == id);
+                if (pessoa == null)
+                    throw new Exception(string.Format("Pessoa com id {0} não encontrada.", id));
+
+                Pessoas.Remove(pessoa);
+                return true;
+            };
+        }
+    }
+
+    public class Pessoa
+    {
+        public long? Id { get; set; }
+        public string Nome { get; set; }
+        public long Cpf { get; set; }
+    }
+}
---
+++ DesenvolvimentoWeb/TrabalhoApresentacao/source/Nancy/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nancy.Hosting.Self;
+using Topshelf;
+
+namespace ApresentacaoNancy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = HostFactory.New(x =>
+            {
+                x.Service<InicializadorServico>(sc =>
+                {
+                    sc.ConstructUsing(name => new InicializadorServico());
+                    sc.WhenStarted((servico, hostControl) =>
+                    {
+                        Task.Factory.StartNew(servico.Iniciar);
+                        return true;
+                    });
+                    sc.WhenStopped((servico, hostControl) =>
+                    {
+                        servico.Parar();
+                        return true;
+                    });
+                });
+                x.RunAsLocalSystem();
+                
+                x.SetDescription("Aplicação para demonstração do NancyFX");
+                x.SetDisplayName("NancyFX");
+                x.SetServiceName("NancyFX");
+
+                x.EnableServiceRecovery(rc => rc.RestartService(delayInMinutes: 1));
+                x.SetStartTimeout(TimeSpan.FromMinutes(1));
+                x.StartAutomatically();
+            });
+
+            host.Run();
+        }
+    }
+
+    internal class InicializadorServico
+    {
+        public void Iniciar()
+        {
+            var hostConfiguration = new HostConfiguration
+            {
+                UrlReservations = new UrlReservations { CreateAutomatically = true }
+            };
+
+            var uri = new Uri("http://localhost:3579");
+            var host = new NancyHost(hostConfiguration, uri);
+            host.Start();
+        }
+
+        public void Parar()
+        {
+        }
+    }
+}
