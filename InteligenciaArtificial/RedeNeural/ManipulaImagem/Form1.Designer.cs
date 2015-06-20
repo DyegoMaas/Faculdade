64ce7716751b0782cf4ba4a8583d7ee4fb5310a7
henriquebertoldi <henriquebertoldi10@gmail.com>
2015-06-20 01:17:32 -0300

    HB:IA:TF: Fiz o projeto pra maniular imagem juntamente. Tem o Form com um campo para escolher a imagem e o desplay pra mostrar a imagem.
    Obs.: pra funcionar o Emgu eu descobri, faz a primeira execução, ai vai dar erro. Com isso criou a pastinha bin/debug no diretório do projeto. Nela tem que colar todas as dlls que estão na pasta Emgu/bin/x6 (pra mim aqui é C:\Emgu\emgucv-windows-universal-cuda 2.4.10.1940\bin\x86), e tem que ser a x86, senão não vai. Além desses arquivos, deve-se copiar e substituir tmb as dlls que foram add com refência no projeto, se encontram no C:\Emgu\bin, a principio são as Emgu.CV, Emgu.CV.UI e Emgu.Util.

---
+++ InteligenciaArtificial/RedeNeural/ManipulaImagem/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ManipulaImagem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.caminhoArquivo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.imagem = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imagem)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arquivo:";
+            // 
+            // caminhoArquivo
+            // 
+            this.caminhoArquivo.Location = new System.Drawing.Point(65, 10);
+            this.caminhoArquivo.Name = "caminhoArquivo";
+            this.caminhoArquivo.ReadOnly = true;
+            this.caminhoArquivo.Size = new System.Drawing.Size(262, 20);
+            this.caminhoArquivo.TabIndex = 1;
+            this.caminhoArquivo.TextChanged += caminhoArquivo_TextChanged;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // imagem
+            // 
+            this.imagem.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imagem.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.imagem.Location = new System.Drawing.Point(16, 39);
+            this.imagem.Name = "pictureBox1";
+            this.imagem.Size = new System.Drawing.Size(540, 334);
+            this.imagem.TabIndex = 3;
+            this.imagem.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 385);
+            this.Controls.Add(this.imagem);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.caminhoArquivo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imagem)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private void caminhoArquivo_TextChanged(object sender, System.EventArgs e)
+        {
+            this.iniciaTratamentoImagem();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox caminhoArquivo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+        private Emgu.CV.UI.ImageBox imagem;
+    }
+}
+
---
+++ InteligenciaArtificial/RedeNeural/ManipulaImagem/Form1.cs
@@ -0,0 +1,148 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using RedeNeural.Core.Classificacao;
+using RedeNeural.Core.Classificacao.Entradas;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManipulaImagem
+{
+    public partial class Form1 : Form
+    {
+        private const int qtdArestas = 32;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var resultado = openFileDialog1.ShowDialog();
+            if (resultado == DialogResult.OK || resultado == DialogResult.Yes)
+            {
+                caminhoArquivo.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void iniciaTratamentoImagem()
+        {
+            //Carrega imagem
+            Image<Bgr, Byte> img =
+                new Image<Bgr, byte>(caminhoArquivo.Text)
+                .Resize(400, 400, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR, true);
+
+            //Converte pra escala de zinza
+            Image<Gray, Byte> gray = img.Convert<Gray, Byte>();
+            CvInvoke.cvCvtColor(img, gray, Emgu.CV.CvEnum.COLOR_CONVERSION.BGR2GRAY);
+
+            CvInvoke.cvThreshold(gray, gray, 127, 255, Emgu.CV.CvEnum.THRESH.CV_THRESH_BINARY);
+            
+            Contour<Point> contornos = gray.FindContours(Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_NONE, Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_TREE);
+
+            var totalPontos = contornos.Total;
+
+            int somatorioX = 0, 
+                somatorioY = 0, 
+                resultadoX, resultadoY;
+
+            for (int i = 0; i < totalPontos; i++)
+            {
+                var ponto = contornos.Pop();
+
+                somatorioX += ponto.X;
+                somatorioY += ponto.Y;
+            }
+
+            resultadoX = somatorioX / totalPontos;
+            resultadoY = somatorioY / totalPontos;
+
+            var centro = new Point(resultadoX, resultadoY);
+            var color = new Bgr(Color.Black);
+            var bitmap = img.ToBitmap();
+            var stepAngular = (360d / qtdArestas);
+
+            List<PointF> pontosEncontrados = new List<PointF>();
+
+            double angulo = 0;
+            while (angulo < 360)
+            {
+                double anguloRad = (angulo * Math.PI / 180);
+
+                const double stepDistancia = 1d;
+                double distancia = 0;
+                while (distancia < 1000)
+                {
+                    var x = distancia * Math.Sin(anguloRad);
+                    var y = distancia * Math.Cos(anguloRad);
+
+                    var pontoB = new Point(centro.X + (int)x, centro.Y + (int)y);
+                    if (EncontrouBorda(pontoB, bitmap))
+                    {
+                        pontosEncontrados.Add(new PointF(pontoB.X, pontoB.Y));
+
+                        CvInvoke.cvLine(img.Ptr, centro, pontoB, new MCvScalar(1), 1, Emgu.CV.CvEnum.LINE_TYPE.CV_AA, 0);
+                        break;
+                    }
+
+                    distancia += stepDistancia;
+                }
+
+                angulo += stepAngular;
+            }
+
+            CvInvoke.cvCircle(img.Ptr, centro, 4, color.MCvScalar, 5, Emgu.CV.CvEnum.LINE_TYPE.CV_AA, 0);
+            foreach (var pontoEncontrado in pontosEncontrados)
+            {
+                var pontoDesenho = new Point((int)pontoEncontrado.X, (int)pontoEncontrado.Y);
+                CvInvoke.cvCircle(img.Ptr, pontoDesenho, 4, new Bgr(Color.Blue).MCvScalar, 5, Emgu.CV.CvEnum.LINE_TYPE.CV_AA, 0);
+            }
+
+            var classificador = ClassificadorAngulos.Abordagem1();
+            var angulos = EncontrarAngulosDosPares(pontosEncontrados);
+            var classificacoes = angulos.Select(a => classificador.Classificar(a)).ToList();
+
+            var geradorValor = new GeradorValoresRede();
+            var bits = geradorValor.GerarValor(classificacoes);
+
+            imagem.Image = img;
+        }
+
+        private IList<int> EncontrarAngulosDosPares(IList<PointF> pontosEncontrados)
+        {
+            var lista = new List<int>();
+            //já temos as boras
+            var extrator = new ExtratorRelacaoAngulos();
+            for (int i = 1; i < pontosEncontrados.Count; i++)
+            {
+                var pontoA = new Vector2(pontosEncontrados[i - 1].X, pontosEncontrados[i - 1].Y);
+                var pontoB = new Vector2(pontosEncontrados[i].X, pontosEncontrados[i].Y);
+
+                var angulo = extrator.ExtrairRelacaoAngulos(new ParAmostral(pontoA, pontoB));
+                lista.Add(angulo);
+            }
+
+            return lista;
+        }
+
+        private bool EncontrouBorda(Point ponto, Bitmap imagem)
+        {
+            var rect = new Rectangle(0, 0, imagem.Width, imagem.Height);
+            if (!rect.Contains(ponto))
+                return false;
+
+            var pixel = imagem.GetPixel(ponto.X, ponto.Y);
+
+            return pixel.R == 0 &&
+                pixel.G == 0 &&
+                pixel.B == 0;
+        }
+    }
+}
